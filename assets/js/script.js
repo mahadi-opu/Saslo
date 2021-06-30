c189c173c897cb76ac36bd6c607de0e4a0a0d91d
Syed Mahadi Hasan Opu <smhasanopu@gmail.com>
2021-06-30 13:09:35 +0600

    File Upload

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+/* -----------------------------------------------------------------------------
+
+
+
+File:           JS Core
+Version:        1.0
+Last change:    00/00/00 
+-------------------------------------------------------------------------------- */
+(function() {
+
+	"use strict";
+
+	var Saslo = {
+		init: function() {
+			this.Basic.init();  
+		},
+
+		Basic: {
+			init: function() {
+
+				this.preloader();
+				this.BackgroundImage();
+				this.StickyHeader();
+				this.MobileMenu();
+				this.scrollTop();
+
+			},
+			preloader: function (){
+				jQuery(window).on('load', function(){
+					jQuery('#preloader').fadeOut('slow',function(){jQuery(this).remove();});
+				})
+			},
+			BackgroundImage: function (){
+				$('[data-background]').each(function() {
+					$(this).css('background-image', 'url('+ $(this).attr('data-background') + ')');
+				});
+			},
+	
+			StickyHeader: function (){
+				jQuery(window).on('scroll', function() {
+					if (jQuery(window).scrollTop() > 250) {
+						jQuery('.saslo-main-header').addClass('sticky-on')
+					} else {
+						jQuery('.saslo-main-header').removeClass('sticky-on')
+					}
+				})
+			},
+			MobileMenu: function (){
+				$('.open_mobile_menu').on("click", function() {
+					$('.mobile_menu_wrap').toggleClass("mobile_menu_on");
+				});
+				$('.open_mobile_menu').on('click', function () {
+					$('body').toggleClass('mobile_menu_overlay_on');
+				});
+				if($('.mobile_menu li.dropdown ul').length){
+					$('.mobile_menu li.dropdown').append('<div class="dropdown-btn"><span class="fas fa-caret-right"></span></div>');
+					$('.mobile_menu li.dropdown .dropdown-btn').on('click', function() {
+						$(this).prev('ul').slideToggle(500);
+					});
+				}
+				$(".dropdown-btn").on("click", function () {
+					$(this).toggleClass("toggle-open");
+				});
+			},
+			scrollTop: function (){
+				$(window).on("scroll", function() {
+					if ($(this).scrollTop() > 250) {
+						$('.scrollup').fadeIn();
+					} else {
+						$('.scrollup').fadeOut();
+					}
+				});
+
+				$('.scrollup').on("click", function()  {
+					$("html, body").animate({
+						scrollTop: 0
+					}, 800);
+					return false;
+				});
+			},
+		
+		}	
+	}
+	jQuery(document).ready(function (){
+		Saslo.init();
+	});
+
+})();
